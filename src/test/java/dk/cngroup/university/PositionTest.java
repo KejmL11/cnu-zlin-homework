3a41c2b246cc075dc387cc818c01d529c0504eca
KejmL11 <kamilsos15@gmail.com>
2018-12-15 12:58:26 +0100

    Implement the rest of getNeighbour and test.

--- src/test/java/dk/cngroup/university/PositionTest.java
+++ src/test/java/dk/cngroup/university/PositionTest.java
@@ -12,4 +12,25 @@ class PositionTest {
 		Position result = position.getNeighbour(Direction.SOUTH);
 		assertEquals(new Position(1,0), result, "New position should be 1,0");
 	}
+
+	@Test
+	void getNeighbourEast() {
+		Position position = new Position(0, 0);
+		Position result = position.getNeighbour(Direction.EAST);
+		assertEquals(new Position(0,1), result, "New position should be 0,1");
+	}
+
+    @Test
+    void getNeighbourNorth() {
+        Position position = new Position(0, 0);
+        Position result = position.getNeighbour(Direction.NORTH);
+        assertEquals(new Position(-1,0), result, "New position should be -1,0");
+    }
+
+    @Test
+    void getNeighbourWest() {
+        Position position = new Position(0, 0);
+        Position result = position.getNeighbour(Direction.WEST);
+        assertEquals(new Position(0,-1), result, "New position should be 0,-1");
+    }
 }
